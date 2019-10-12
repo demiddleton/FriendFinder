cd3e1566eddb040d642ca64795c22529773796e4
Denise Middleton <demiddleton@bellsouth.net>
2019-10-12 16:05:49 -0400

    added 3 friends in the friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,10 +5,19 @@ var path = require("path");
 // =============================================================
 var friends = [
     {
-        name: " ",
-        photo: " ",
-        scores: []
-
+        name: "Flava Fav",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy0cHrZ1UWXk04c7EC55FdhQtv5Ni8dWkv8ifQ2bWO22ZYvkUt_w",
+        scores: [5, 1, 3, 1, 4, 5, 5, 5, 2, 5]
+    },
+    {
+        name: "Superman",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb_gHug7zrwsiDmtI9wcA4yqPn6pV1hoYk4Yxr7OG9sG-KN7_MXQ",
+        scores: [1, 5, 4, 4, 5, 2, 5, 5, 5, 3]
+    },
+    {
+        name: "Denzel Washington"
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSC6C0sp7tSizST6kl4aZ4MKanzEwHyEBdKRA_gbcNySTM0lcU",
+        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
     }
 ];
 
