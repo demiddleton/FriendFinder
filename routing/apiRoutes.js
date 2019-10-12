e201d9ff58b554e054c312fbacc0526e8c8fe9ae
Denise Middleton <demiddleton@bellsouth.net>
2019-10-12 16:27:19 -0400

    created code to grab(get) the friends data and format it as json data

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -3,3 +3,11 @@
 var express = require("express");
 var path = require("path");
 // =============================================================
+
+var friends = require("app\data\friends.js");
+
+module.exports = function (app) {
+    app.get("/api/friends", function(req, res) {
+res.json(friends);
+    });
+};
