3e3254c020b9e4df1e78746ad2d85d556048709f
Denise Middleton <demiddleton@bellsouth.net>
2019-11-05 18:47:37 -0500

    updated api routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,11 +7,11 @@ var path = require("path");
 module.exports = function(app) {
 
     app.get("/home", function(req,res) {
-        res.sendfile(path.join(__dirname, "../public/home.html"));
+        res.sendfile(path.join(__dirname, "/../public/home.html"));
     });
 
-    app.get("/survey", function(req,res) {
-        res.sendfile(path.join(__dirname, "../public/survey.html"));
+    app.use("/survey", function(req,res) {
+        res.sendfile(path.join(__dirname, "/../public/survey.html"));
     });
 
     // If no matching route is found default to home
--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(express.json());
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
+
 //Listener
 // app.listen(PORT, function() {
 //     console.log("App listening on PORT: " + PORT);
