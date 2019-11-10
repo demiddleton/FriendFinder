ee433602d3cbd5af8a577aeeba407cc5a8ad77a1
Denise Middleton <demiddleton@bellsouth.net>
2019-11-10 07:52:48 -0500

    updated modal

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,29 @@
-// Dependencies
-// =============================================================
-const express = require("express");
 const path = require("path");
-// =============================================================
 
 const friends = require("../app/data/friends");
 
-module.exports = function (app) {
-	// Total list of friend entries
-	app.get('/api/friends', function (req, res) {
-		res.json(friends);
-	});
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+  app.post("/api/friends", function(req, res) {
+    var newFriend = req.body;
+    var bestFriend;
+    var maxDiff = 40;
+    for (var i = 0; i < friends.length; i++) {
+      var friendDiff = 0;
+      for (var j = 0; j < friends[i].scores.length; j++) {
+        var friendScore = friends[i].scores[j];
+        friendDiff += Math.abs(friendScore - parseInt(newFriend.scores));
+      }
+      // if current friend being looped has a lower diff than maxDiff change maxDiff and the bestFriend
+      if (friendDiff < maxDiff) {
+        maxDiff = friendDiff;
+        bestFriend = friends[i];
+      }
+    }
 
-	app.post("/api/friends", function (req, res) {
-		var newFriend = req.body;
-		var bestFriend;
-		var maxDiff = 40;
-		for (var i = 0; i < friends.length; i++) {
-			var friendDiff = 0;
-			for (var j = 0; j < friends[i].scores.length; j++) {
-				var friendScore = friends[i].scores[j];
-				friendDiff += Math.abs(friendScore - parseInt(newFriend.scores));
-			}
-			// if current friend being looped has a lower diff than minDiff change minDiff and the bestFriend
-			if (friendDiff < maxDiff) {
-				maxDiff = friendDiff;
-				bestFriend = friends[i];
-			}
-		}
-
-		res.json(bestFriend);
-		friends.push(newFriend);
-	});
+    res.json(bestFriend);
+    friends.push(newFriend);
+  });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,18 +1,11 @@
-// Dependencies
-// =============================================================
-var express = require("express");
 var path = require("path");
-// =============================================================
 
 module.exports = function(app) {
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
 
-   app.get("/survey", function(req,res) {
-        res.sendfile(path.join(__dirname, "../public/survey.html"));
-    });
-
-    // If no matching route is found default to home
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
-
 };
